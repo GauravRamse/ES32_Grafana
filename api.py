e3ab1ff0c9b6c029069a74862f9120aa3f929425
Gaurav Ramse <56560538+GauravRamse@users.noreply.github.com>
2024-05-05 09:00:26 +0200

    api.py

--- api.py
+++ api.py
@@ -2,10 +2,15 @@ import os
 import databases
 from config import DB_URI
 from datetime import datetime
+import numpy as np
 
-
+from functions import *
 
 from typing import Optional
+import pandas as pd
+# Load the saved model
+import xgboost as xgb
+
 
 database = databases.Database(DB_URI)
 
@@ -15,6 +20,49 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 
 
+
+# Function to connect to the database
+@app.on_event("startup")
+async def connect_to_database():
+    await database.connect()
+    print("Connected to database")
+
+
+async def update_into_table():
+        
+
+    loaded_model = xgb.XGBRegressor()
+    loaded_model.load_model(r'model/xgboost_model.model')
+
+    results = await database.fetch_all("select * from hackupc.dht11")
+    d = [dict(result._mapping) for result in results]
+    df = pd.DataFrame.from_dict(d)
+    df.set_index(['Id', 'dt'], inplace=True)  # Set 'Id' and 'dt' as index
+
+    # Reset index to convert the multi-index into regular columns
+    df.reset_index(inplace=True)
+
+    # Group by 'Id', date, and hour, then aggregate temperature using min and max
+    new_df = df.groupby([df['Id'], df['dt'].dt.date.rename('dates'), df['dt'].dt.hour.rename('hour')]).agg(
+        min_temperature=('temperature', 'min'),
+        max_temperature=('temperature', 'max'),
+        photoresistor=('photoresistor', 'first')
+    ).reset_index()
+
+    a = []
+    for i, row in new_df.iterrows():
+        row_dict = row.to_dict()
+            
+        # Assuming you have two new data points as a list of lists or a numpy array
+        new_data = np.array([[row_dict["min_temperature"], row_dict["max_temperature"]]])
+
+        # Make predictions using the loaded model for the new data
+        predictions = loaded_model.predict(new_data)
+        
+        a.append({"Id": row_dict["Id"], "precipitation_pred": predictions[0]})
+
+    for res in a:
+        await  update_into_table_multiple_where(database, "hackupc.dht11", res, ["Id"])
 
 async def insert_into_table(database, table_name ,values_in_dict):
     """
@@ -47,11 +95,6 @@ async def insert_into_table(database, table_name ,values_in_dict):
 
 
 
-# Function to connect to the database
-@app.on_event("startup")
-async def connect_to_database():
-    await database.connect()
-    print("Connected to database")
 
 @app.get("/get_data/")
 async def root():
@@ -60,16 +103,22 @@ async def root():
 
 @app.post("/post_data/")
 async def post_data(info: Request):
+    
+    current_minute = datetime.now().minute
+    
+    # Check if current minute is divisible by 10
+    if current_minute % 2 == 0:
+        # Call your function here
+        await update_into_table()
+        
     value_dict = await info.json()
-
     # await database.connect()
     if value_dict is not None:
         formatted_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         value_dict["dt"] = formatted_date
-        
         pk = await insert_into_table(database, "hackupc.dht11" , value_dict )
         
         return {"pk": pk}
     else:
-        return {"message": f"Received temperature"}
+        return {"message": f"Received temperature None"}
